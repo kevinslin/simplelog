4f449aa3290a83fe1471ae980605fc4152ef88f1
Kevin Lin <kevinslin8@gmail.com>
2013-03-26 21:58:31 -0500

    fix bug in date formatter

--- simplelog/settings.py
+++ simplelog/settings.py
@@ -4,5 +4,5 @@ from logging import NOTSET, INFO, DEBUG, WARNING, ERROR, CRITICAL
 SIMPLELOG = "simplelog"
 SIMPLE_FORMAT = "[%(levelname)s]: %(asctime)s: %(message)s"
 DETAIL_FORMAT = "[%(levelname)s], %(filename)s:%(funcName)s(), %(asctime)s: %(message)s"
-FMT_DATETIME = "%H:%m.%S" # hour:minute.seconds
+FMT_DATETIME = "%m/%d/%Y-%H:%M.%S" # hour:minute.seconds
 SIMPLE_FORMATTER = logging.Formatter(fmt = SIMPLE_FORMAT, datefmt = FMT_DATETIME)
